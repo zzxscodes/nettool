b2a5ad69734c38b50d156c946e8df6a5acddbb82
zzxscodes <129288947+zzxscodes@users.noreply.github.com>
2025-03-30 23:37:24 +0800

    Update sockredirect.h

--- sockredirect.h
+++ sockredirect.h
@@ -1,8 +1,17 @@
 #ifndef __SOCKREDIRECT_H
 #define __SOCKREDIRECT_H
 
+#ifndef ETH_P_IP
+#define ETH_P_IP 0x0800
+#endif
+
+#ifndef ETH_P_IPV6
+#define ETH_P_IPV6 0x86DD
+#endif
+
 struct sock_key {
-    __u16 family; // Address family (AF_INET or AF_INET6)
+    __u16 family;   // Address family (AF_INET or AF_INET6)
+    __u8 protocol;  // IPPROTO_TCP or IPPROTO_UDP
     union {
         __u32 src_ip4;
         __u32 src_ip6[4];
